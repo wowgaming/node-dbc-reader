a23ad7a173d39d08bb891f776fa94a7c432839c3
Yehonal <yehonal.azeroth@gmail.com>
2021-04-21 17:08:08 +0200

    2 minor fixes

--- apps/schema-generator/index.js
+++ apps/schema-generator/index.js
@@ -66,6 +66,8 @@ async function main() {
     
     fs.writeFileSync(`output/${tableList[table.name]}.json`, JSON.stringify(dbcSchema, null, 2));
   }
+
+  console.log("Schemas exported! Check the output folder");
 }
 
 main();
